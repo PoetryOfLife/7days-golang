4a2be22101a00bfbdfdc7a6fbb296e16f2eb4464
zengzhengxiong <zengzhengxiong050@chinasoftinc.com>
2023-03-21 15:21:12 +0800

    前缀树

--- zed/Trie.go
+++ zed/Trie.go
@@ -6,3 +6,24 @@ type node struct {
 	children []*node
 	isWild   bool
 }
+
+// 第一个匹配成功的节点，用于插入
+func (n *node) matchChild(part string) *node {
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			return child
+		}
+	}
+	return nil
+}
+
+//所有匹配成功的节点，用于查找
+func (n *node) matchChildren(part string) []*node {
+	nodes := make([]*node, 0)
+	for _, child := range n.children {
+		if child.part == part || child.isWild {
+			nodes = append(nodes, child)
+		}
+	}
+	return nil
+}
